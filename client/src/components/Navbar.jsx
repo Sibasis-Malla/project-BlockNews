66d46de6ffb888a239422548be4d6cd193bf9a35
Sambit Sankalp <sambit.sankalp.official@gmail.com>
2022-08-21 13:20:29 +0530

    fix:mui error  fixed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,39 +2,30 @@ import React, { useContext } from 'react';
 import Web3Context from '../contexts';
 import { NavLink } from 'react-router-dom';
 import { Typography, Button } from '@mui/material';
-import { makeStyles } from '@mui/styles';
 import { Add } from '@mui/icons-material';
 
 const Navbar = () => {
-  const classes = useStyles();
   const { connectWallet, account } = useContext(Web3Context);
-  // const [user, setuser] = useState("+ Connect The Wallet");
-  // const [connected, setconnected] = useState(false);
-  // function setData() {
-  //   setconnected(true);
-  //   if(connected){
-  //     setuser("Hey, 0x67d36FB0b3b6a1cC11343d17646A5D9c94a2d098");
-  //   }
-  //   else{
-  //     setuser("+ Connect The Wallet");
-  //   }
-  //   console.log(connected, user);
-  // }
   return (
     <>
       <div className="flex justify-between h-16 bg-nav">
         <div className="mt-4 ml-24">
-          <NavLink to="/" className="flex jusitify-center align-center w-10 h-auto">
+          <NavLink
+            to="/"
+            className="flex jusitify-center align-center w-10 h-auto"
+          >
             <img
               src="https://res.cloudinary.com/sambitsankalp/image/upload/v1660398315/hackathons/ezgif_1_tmlyl3.png"
               alt="block"
             />
-            <span className="mt-1 font-bold text-xl text-textcol z-50 opacity-100">BlockNews</span>
+            <span className="mt-1 font-bold text-xl text-textcol z-50 opacity-100">
+              BlockNews
+            </span>
           </NavLink>
         </div>
-        <div className={classes.tabsContainer}>
+        <div className="flex justify-center items-center mr-14">
           {account.currentAccount != null ? (
-            <Typography className={classes.tab} variant="body1">
+            <Typography className="mr-20" style={{color:'#fffffe'}} variant="body1">
               Hey,{' '}
               {`${String(account.currentAccount).slice(0, 5)}...${String(
                 account.currentAccount
@@ -48,7 +39,7 @@ const Navbar = () => {
               onClick={connectWallet}
             >
               <Typography
-                className={classes.tab}
+                className="mr-20"
                 style={{ color: '#ffffff' }}
                 variant="body1"
               >
@@ -63,48 +54,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '100%',
-    height: '70px',
-    position: 'fixed',
-    top: 0,
-    backgroundColor: '#232946',
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    zIndex: 1,
-  },
-  logoContainer: {
-    textDecoration: 'none',
-    marginLeft: '10%',
-  },
-  logo: {
-    color: '#fff',
-    textTransform: 'uppercase',
-  },
-  tabsContainer: {
-    display: 'flex',
-    justifyContent: 'center',
-    marginRight: '10%',
-    alignItems: 'center',
-    [theme.breakpoints.down('xs')]: {
-      display: 'none',
-    },
-  },
-  tabLink: {
-    margin: 0,
-    textDecoration: 'none',
-    padding: 'auto 15px',
-    marginLeft: '10px',
-    [theme.breakpoints.down('sm')]: {
-      width: '100%',
-    },
-  },
-  tab: {
-    color: '#fffffe',
-    opacity: 1,
-    fontFamily: 'Helvetica',
-    marginRight: '20px',
-  },
-}));
+
--- client/src/index.js
+++ client/src/index.js
@@ -4,19 +4,13 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import Web3Provider from "./contexts/Web3Provider";
-import { ThemeProvider } from "@mui/styles";
-import { createTheme, responsiveFontSizes } from "@mui/material/styles";
-let theme = createTheme();
-theme = responsiveFontSizes(theme);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Web3Provider>
-        <ThemeProvider theme={theme}>
           <App />{" "}
-        </ThemeProvider>
       </Web3Provider>
     </BrowserRouter>
   </React.StrictMode>
