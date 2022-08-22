0858431c68a733649a66613865f07dc589b419ab
Sibasis-Malla <sibasisml@gmail.com>
2022-08-23 02:37:48 +0530

    Fix:voting page

--- client/src/pages/TweetVote.jsx
+++ client/src/pages/TweetVote.jsx
@@ -151,14 +151,14 @@ function TweetVote() {
                           className="w-full mr-2 text-white bg-green-500 hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                           onClick={handleAgree}
                         >
-                          Agree
+                          Not Fake
                         </button>
                         <button
                           type="submit"
                           className="w-full ml-2 text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                           onClick={handleDisAgree}
                         >
-                          Disagree
+                          Fake
                         </button>
                       </div>
                     </>
